76cd77ff95a08ef15bcfe66604822ca7c173ebe8
Ruzbel <bengy186@hotmail.com>
2017-05-04 09:59:17 -0500

    final
    terminado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 pantalla=document.getElementById('display')
 var acum=""
 var acum2=0
+var acumres=0
+var cont=0
 var cadena=""
 pantalla.innerHTML="0"
 uno=document.getElementById('1')
@@ -24,220 +26,207 @@ divi=document.getElementById('dividido')
  function cambioTecla() {
    teclas[0].style="width:20%;height:59px;margin:0% 1%"}
  function vuelveTecla() {
-   teclas[0].style="width:22%;height:62.91px;margin:0%"}
-teclas[0].onmousedown=cambioTecla;
-teclas[0].onmouseup=vuelveTecla;
+  teclas[0].style="width:22%;height:62.91px;margin:0%"}
+  teclas[0].onmousedown=cambioTecla;
+  teclas[0].onmouseup=vuelveTecla;
 function cambioTecla1() {
   teclas[1].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla1() {
   teclas[1].style="width:22%;height:62.91px;margin:0%"}
-teclas[1].onmousedown=cambioTecla1;
-teclas[1].onmouseup=vuelveTecla1;
+  teclas[1].onmousedown=cambioTecla1;
+  teclas[1].onmouseup=vuelveTecla1;
 function cambioTecla2() {
   teclas[2].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla2() {
   teclas[2].style="width:22%;height:62.91px;margin:0% "}
-teclas[2].onmousedown=cambioTecla2;
-teclas[2].onmouseup=vuelveTecla2;
+  teclas[2].onmousedown=cambioTecla2;
+  teclas[2].onmouseup=vuelveTecla2;
 function cambioTecla3() {
   teclas[3].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla3() {
   teclas[3].style="width:22%;height:62.91px;margin:0%"}
-teclas[3].onmousedown=cambioTecla3;
-teclas[3].onmouseup=vuelveTecla3;
+  teclas[3].onmousedown=cambioTecla3;
+  teclas[3].onmouseup=vuelveTecla3;
 function cambioTecla4() {
   teclas[4].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla4() {
   teclas[4].style="width:22%;height:62.91px;margin:0%"}
-teclas[4].onmousedown=cambioTecla4;
-teclas[4].onmouseup=vuelveTecla4;
+  teclas[4].onmousedown=cambioTecla4;
+  teclas[4].onmouseup=vuelveTecla4;
 function cambioTecla5() {
   teclas[5].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla5() {
   teclas[5].style="width:22%;height:62.91px;margin:0%"}
-teclas[5].onmousedown=cambioTecla5;
-teclas[5].onmouseup=vuelveTecla5;
+  teclas[5].onmousedown=cambioTecla5;
+  teclas[5].onmouseup=vuelveTecla5;
 function cambioTecla6() {
   teclas[6].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla6() {
   teclas[6].style="width:22%;height:62.91px;margin:0%"}
-teclas[6].onmousedown=cambioTecla6;
-teclas[6].onmouseup=vuelveTecla6;
+  teclas[6].onmousedown=cambioTecla6;
+  teclas[6].onmouseup=vuelveTecla6;
 function cambioTecla7() {
   teclas[7].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla7() {
   teclas[7].style="width:22%;height:62.91px;margin:0%"}
-teclas[7].onmousedown=cambioTecla7;
-teclas[7].onmouseup=vuelveTecla7;
+  teclas[7].onmousedown=cambioTecla7;
+  teclas[7].onmouseup=vuelveTecla7;
 function cambioTecla8() {
   teclas[8].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla8() {
   teclas[8].style="width:22%;height:62.91px;margin:0%"}
-teclas[8].onmousedown=cambioTecla8;
-teclas[8].onmouseup=vuelveTecla8;
+  teclas[8].onmousedown=cambioTecla8;
+  teclas[8].onmouseup=vuelveTecla8;
 function cambioTecla9() {
   teclas[9].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla9() {
   teclas[9].style="width:22%;height:62.91px;margin:0%"}
-teclas[9].onmousedown=cambioTecla9;
-teclas[9].onmouseup=vuelveTecla9;
+  teclas[9].onmousedown=cambioTecla9;
+  teclas[9].onmouseup=vuelveTecla9;
 function cambioTecla10() {
   teclas[10].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla10() {
   teclas[10].style="width:22%;height:62.91px;margin:0%"}
-teclas[10].onmousedown=cambioTecla10;
-teclas[10].onmouseup=vuelveTecla10;
+  teclas[10].onmousedown=cambioTecla10;
+  teclas[10].onmouseup=vuelveTecla10;
 function cambioTecla11() {
   teclas[11].style="width:20%;height:59px;margin:0% 1%"}
 function vuelveTecla11() {
   teclas[11].style="width:22%;height:62.91px;margin:0%"}
-teclas[11].onmousedown=cambioTecla11;
-teclas[11].onmouseup=vuelveTecla11;
+  teclas[11].onmousedown=cambioTecla11;
+  teclas[11].onmouseup=vuelveTecla11;
 function cambioTecla12() {
   teclas[12].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla12() {
   teclas[12].style="width:29%;height:62.91px;margin:0%"}
-teclas[12].onmousedown=cambioTecla12;
-teclas[12].onmouseup=vuelveTecla12;
+  teclas[12].onmousedown=cambioTecla12;
+  teclas[12].onmouseup=vuelveTecla12;
 function cambioTecla13() {
   teclas[13].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla13() {
   teclas[13].style="width:29%;height:62.91px;margin:0%"}
-teclas[13].onmousedown=cambioTecla13;
-teclas[13].onmouseup=vuelveTecla13;
+  teclas[13].onmousedown=cambioTecla13;
+  teclas[13].onmouseup=vuelveTecla13;
 function cambioTecla14() {
   teclas[14].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla14() {
   teclas[14].style="width:29%;height:62.91px;margin:0%"}
-teclas[14].onmousedown=cambioTecla14;
-teclas[14].onmouseup=vuelveTecla14;
+  teclas[14].onmousedown=cambioTecla14;
+  teclas[14].onmouseup=vuelveTecla14;
 function cambioTecla15() {
   teclas[15].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla15() {
   teclas[15].style="width:29%;height:62.91px;margin:0%"}
-teclas[15].onmousedown=cambioTecla15;
-teclas[15].onmouseup=vuelveTecla15;
+  teclas[15].onmousedown=cambioTecla15;
+  teclas[15].onmouseup=vuelveTecla15;
 function cambioTecla16() {
   teclas[16].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla16() {
   teclas[16].style="width:29%;height:62.91px;margin:0%"}
-teclas[16].onmousedown=cambioTecla16;
-teclas[16].onmouseup=vuelveTecla16;
+  teclas[16].onmousedown=cambioTecla16;
+  teclas[16].onmouseup=vuelveTecla16;
 function cambioTecla17() {
   teclas[17].style="width:27%;height:59px;margin:0% 1%"}
 function vuelveTecla17() {
   teclas[17].style="width:29%;height:62.91px;margin:0%"}
-teclas[17].onmousedown=cambioTecla17;
-teclas[17].onmouseup=vuelveTecla17;
+  teclas[17].onmousedown=cambioTecla17;
+  teclas[17].onmouseup=vuelveTecla17;
 function cambioTecla18() {
   teclas[18].style="width:86%;margin:2%;margin-left:10px;margin-bottom:3px"}
 function vuelveTecla18() {
   teclas[18].style="width:90%;margin:0px:margin-left:10px"}
-teclas[18].onmousedown=cambioTecla18;
-teclas[18].onmouseup=vuelveTecla18;
+  teclas[18].onmousedown=cambioTecla18;
+  teclas[18].onmouseup=vuelveTecla18;
 
 on.addEventListener("click",function(){
-borra()
-})
+  borra()})
 signo.addEventListener("click",function(){
-cadena=validaSigno(cadena)
-})
+  cadena=validaSigno(cadena)})
 uno.addEventListener('click', function(){
-cadena=digitar(cadena,"1")
-})
+  cadena=digitar(cadena,"1")})
 dos.addEventListener('click', function(){
-cadena=digitar(cadena,"2")
-})
+  cadena=digitar(cadena,"2")})
 tres.addEventListener('click', function(){
-cadena=digitar(cadena,"3")
-})
+  cadena=digitar(cadena,"3")})
 cuatro.addEventListener('click', function(){
-cadena=digitar(cadena,"4")
-})
+  cadena=digitar(cadena,"4")})
 cinco.addEventListener('click', function(){
-cadena=digitar(cadena,"5")
-})
+  cadena=digitar(cadena,"5")})
 seis.addEventListener('click', function(){
-cadena=digitar(cadena,"6")
-})
+  cadena=digitar(cadena,"6")})
 siete.addEventListener('click', function(){
-cadena=digitar(cadena,"7")
-})
+  cadena=digitar(cadena,"7")})
 ocho.addEventListener('click', function(){
-cadena=digitar(cadena,"8")
-})
+  cadena=digitar(cadena,"8")})
 nueve.addEventListener('click', function(){
-cadena=digitar(cadena,"9")
-})
+  cadena=digitar(cadena,"9")})
 cero.addEventListener('click', function(){
-    cadena=comprueba0(cadena)
-})
+  cadena=comprueba0(cadena)})
 punto.addEventListener('click', function(){
-cadena=compruebaPunto(cadena)
-})
+  cadena=compruebaPunto(cadena)})
 suma.addEventListener('click', function(){
-operacion=sumar()
-})
+  operacion=sumar()})
 resta.addEventListener('click', function(){
-operacion=restar()
-})
+  operacion=restar()})
 multi.addEventListener('click', function(){
-operacion=multiplicar()
-})
+  operacion=multiplicar()})
 divi.addEventListener('click', function(){
-operacion=dividir()
-})
+  operacion=dividir()})
 igual.addEventListener('click', function(){
-
- cadena=resultado(operacion)
-
-})
+  cadena=resultado(operacion[0],operacion[1])})
 var borrar=""
-function resultado(operacion){
-
-if (operacion=="+") {
-  var r=parseFloat(acum2) + parseFloat(acum)
-}else if (operacion=="-") {
+function resultado(operacion,repite){
+  if (operacion=="+") {
+    var r=parseFloat(acum) + parseFloat(acum2)
+  }else if (operacion=="-") {
     var r=parseFloat(acum2) - parseFloat(acum)
-}else if (operacion=="*") {
+  }else if (operacion=="*") {
     var r=parseFloat(acum2) * parseFloat(acum)
-}else if (operacion=="/") {
+  }else if (operacion=="/") {
     var r=parseFloat(acum2)/parseFloat(acum)
-}
-r=r.toString()
-acum=r
-if (r.length<9) {
-  pantalla.innerHTML=r
-}else{
-  pantalla.innerHTML=r[0]+r[1]+r[2]+r[3]+r[4]+r[5]+r[6]+r[7]
- }
-    borrar=1
-    return r
+  }
+  r=r.toString()
+  if (repite>=acumres){
+    acum2=r
+  }else{
+    acum=r
+  }
+  if (r.length<9) {
+    pantalla.innerHTML=r
+  }else{
+    pantalla.innerHTML=r[0]+r[1]+r[2]+r[3]+r[4]+r[5]+r[6]+r[7]
+  }
+  borrar=1
+  acumres=repite
+  return r
 }
 function sumar(){
-pantalla.innerHTML=""
-acum2=acum
-acum=""
-
-return "+"
+  acum2=pantalla.innerHTML
+  pantalla.innerHTML=""
+  acum=""
+  cont=cont+1
+  return ("+"+cont)
 }
 function restar(){
-pantalla.innerHTML=""
-acum2=acum
-acum=""
-return "-"
+  acum2=pantalla.innerHTML
+  pantalla.innerHTML=""
+  acum=""
+  cont=cont+1
+return ("-"+cont)
 }
 function multiplicar(){
-pantalla.innerHTML=""
-acum2=acum
-acum=""
-return "*"
+  acum2=pantalla.innerHTML
+  pantalla.innerHTML=""
+  acum=""
+  cont=cont+1
+  return ("*"+cont)
 }
 function dividir(){
-pantalla.innerHTML=""
-acum2=acum
-acum=""
-return "/"
+  acum2=pantalla.innerHTML
+  pantalla.innerHTML=""
+  acum=""
+  cont=cont+1
+  return ("/"+cont)
 }
 
 function digitar (cadena,num){
@@ -274,6 +263,7 @@ function borra() {
   cadena=""
   acum=""
   acum2=0
+  operacion=0
 }
 function validaSigno(cadena){
   acum=cadena*-1
